37998f4e9fd8d2a42fbd33d4bef45a1b2c7a14a9
Neil Martin <nnmartin94@gmail.com>
2020-09-14 00:21:15 -0700

    removed unused main file

--- graph/domain/mutation/logInUser.go
+++ graph/domain/mutation/logInUser.go
@@ -1,5 +1,14 @@
 package mutation
 
-func LogInUser() {
+import "fmt"
 
+// LogInResponse consists of the logged in User as well as the token
+type LogInResponse struct {
+}
+
+// LogInUser logs in a user by comparing password hashes and returning a token
+func LogInUser() LogInResponse {
+	// TODO
+	fmt.Println("reached login service")
+	return LogInResponse{}
 }
