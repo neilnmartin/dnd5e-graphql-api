c800de1daa4b374d0d847ecf32e4bdc92aa8fbc5
Neil Martin <nnmartin94@gmail.com>
2020-09-15 00:39:44 -0700

    added some more types to the domain types

--- graph/domain/main.go
+++ graph/domain/main.go
@@ -1,6 +1,6 @@
 package domain
 
-// define server types
+// This is where the actual domain types will be defined (as opposed to the types exposed on the schema)
 
 // User describes the user of the application
 type User struct {
@@ -14,6 +14,34 @@ type User struct {
 type Name struct {
 	GivenName  string
 	FamilyName string
+}
+
+// Character describes a user's DnD 5E character
+type Character struct {
+	ID   string
+	Name string // character name
+	Race
+	Class
+	level      int8 // small int max 128
+	Equipments []Equipment
+}
+
+// Race describes the DnD 5E character Race
+type Race struct {
+	ID   string
+	Name string
+}
+
+// Class describes the character class
+type Class struct {
+	ID   string
+	Name string
+}
+
+// Equipment describes a piece or item of equipment
+type Equipment struct {
+	ID   string
+	Name string
 }
 
 // UserRepo describes the user repository with User related methods
