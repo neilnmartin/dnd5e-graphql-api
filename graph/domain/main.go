d1370dccd9f37ab46d18e008dab533eb80d850ee
Neil Martin <nnmartin94@gmail.com>
2020-09-14 21:52:20 -0700

    moved mutation logic to applciation package

--- graph/domain/main.go
+++ graph/domain/main.go
@@ -1,3 +1,22 @@
 package domain
 
 // define server types
+
+// User describes the user of the application
+type User struct {
+	ID       string
+	Email    string
+	Password string
+	Name
+}
+
+// Name type describes the name of the user. Follows SCIM attributes.
+type Name struct {
+	GivenName  string
+	FamilyName string
+}
+
+// UserRepo describes the user repository with User related methods
+type UserRepo interface {
+	CreateUser()
+}
