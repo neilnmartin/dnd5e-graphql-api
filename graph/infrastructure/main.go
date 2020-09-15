f987ed515ce13a6e68a7ccbe7d6d78e61066b57c
Neil Martin <nnmartin94@gmail.com>
2020-09-14 22:58:15 -0700

    added user repo to infrastructure

--- graph/infrastructure/main.go
+++ graph/infrastructure/main.go
@@ -2,24 +2,21 @@ package infrastructure
 
 import (
 	"github.com/neilnmartin/dnd5e-graphql-api/graph/domain"
+	"gopkg.in/mgo.v2"
 )
 
 // A Datasource that implements a data repository with CRUD for each resource
 type Datasource interface {
 	CreateUser(domain.User) domain.User
 	// ReadUser(domain.User) domain.User
-	// UpdateUser(domain.User) domain.User
-	// DeleteUser(domain.User) domain.User
+}
 
-	CreateGame()
-	ReadGame()
-	UpdateGame()
-	DeleteGame()
-
-	CreateCharacter()
-	ReadCharacter()
-	UpdateCharacter()
-	DeleteCharacter()
+// MongoUserRepo is the mongodb infrastructure implementation of the UserRepo domain interface
+type MongoUserRepo interface {
+	CreateUser(domain.User) domain.User
+	GetUserByEmail(domain.User) domain.User
+	UpdateUser(domain.User) domain.User
+	DeleteUser(domain.User) domain.User
 }
 
 // DatasourceFactory creates a Datasource interface
@@ -31,5 +28,17 @@ func DatasourceFactory(p string) Datasource {
 	// default to mongo
 	default:
 		return createMongoDataSource()
+	}
+}
+
+func createMongoDataSource() mongoDatasource {
+
+	session, err := mgo.Dial("127.0.0.1:27017")
+	if err != nil {
+		panic(err)
+	}
+
+	return mongoDatasource{
+		session,
 	}
 }
