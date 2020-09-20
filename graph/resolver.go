d204d34acb7c544bac5124f5ba92c7a05b5ff6a6
Neil Martin <nnmartin94@gmail.com>
2020-09-19 22:32:01 -0700

    updated resolver type to have race

--- graph/resolver.go
+++ graph/resolver.go
@@ -2,36 +2,52 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/neilnmartin/dnd5e-graphql-api/graph/model"
+	"github.com/neilnmartin/dnd5e-graphql-api/graph/repository"
 )
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
+var db = repository.DB
+
+// Resolver struct holds general resolvers
 type Resolver struct {
 }
 
 // Character finds a character
-func (r *Resolver) Character(ctx context.Context) (*model.Character, error) {
-	fmt.Printf("%v", "character resolver hit")
-	panic(fmt.Errorf("not implemented"))
+// func (r *Resolver) Character(ctx context.Context) (*model.Character, error) {
+// 	fmt.Printf("%v", "character resolver hit")
+// 	panic(fmt.Errorf("not implemented"))
+// }
+
+//RaceInput for Race resolver
+type RaceInput struct {
+	name string `json:"name"`
 }
 
-// func (r *Resolver) Race(ctx context.Context) (*model.Race, error) {
-// 	var rr domain.Race
-// 	if input.id != nil {
-// 		rr = repository.DB.GetRaceById(input.id)
-// 		return race, nil
-// 	} else if input.name != nil {
-// 		rr = repository.DB.GetRaceByName(input.name)
-// 		return race, nil
-// 	} else {
-// 		return nil, errors.New("Could not find Race")
-// 	}
-// }
+//Race generic resolver
+func (r *Resolver) Race(ctx context.Context, input RaceInput) (*model.Race, error) {
+	race, err := repository.DB.RaceRepo.GetRaceByName(input.name)
+	if err != nil {
+		return nil, err
+	}
+	return &model.Race{
+		ID:                  race.ID,
+		Name:                race.Name,
+		Age:                 &race.Age,
+		Size:                &race.Size,
+		SizeDescription:     &race.SizeDescription,
+		Speed:               &race.Speed,
+		LanguageDescription: &race.LanguageDescription,
+		// StartingProficiencies
+		// Traits
+		// SubRaces
+	}, nil
+}
+
 // func (r *Resolver) Class(ctx context.Context) (*model.Class, error) {
 // 	panic(fmt.Errorf("not implemented"))
 // }
