9cb02bab1450ec7aa2a19e0a616d95be02f6fec7
Neil Martin <nnmartin94@gmail.com>
2020-09-22 00:00:51 -0700

    refactored sign up service

--- graph/application/signUpUserService.go
+++ graph/application/signUpUserService.go
@@ -12,38 +12,38 @@ import (
 
 // SignUpUserService signs up a user by adding to the database after validating name email and password inputs
 func SignUpUserService(email string, password string, name domain.Name, ur domain.UserRepo) (*domain.User, error) {
+	log.Println("\nhit signup service")
 	// check existing user: TODO
-	eu, err := ur.GetUserByEmail(email)
+	_, err := ur.GetUserByEmail(email)
 	if err != nil {
+		if err == domain.ErrUserNotFound {
+			validEmail := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+			if !validEmail.MatchString(email) {
+				log.Fatal("not a valid email")
+				return nil, errors.New("Invalid email")
+			}
+
+			hpw, err := bcrypt.GenerateFromPassword([]byte(password), 5)
+			if err != nil {
+				log.Printf(err.Error())
+			}
+
+			shpw := string(hpw)
+			fmt.Printf("%v original pw, %v stringified hashed pw", password, shpw)
+
+			nu := domain.User{
+				Email:    email,
+				Password: shpw,
+				Name: domain.Name{
+					GivenName:  name.GivenName,
+					FamilyName: name.FamilyName,
+				},
+			}
+
+			user, err := ur.InsertUser(nu)
+			return user, nil
+		}
 		return nil, err
 	}
-	if eu != nil {
-		return nil, errors.New("A user with this email already exists")
-	}
-
-	validEmail := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
-	if !validEmail.MatchString(email) {
-		log.Fatal("not a valid email")
-		return nil, errors.New("Invalid email")
-	}
-
-	hpw, err := bcrypt.GenerateFromPassword([]byte(password), 5)
-	if err != nil {
-		log.Printf(err.Error())
-	}
-
-	shpw := string(hpw)
-	fmt.Printf("%v original pw, %v stringified hashed pw", password, shpw)
-
-	nu := domain.User{
-		Email:    email,
-		Password: shpw,
-		Name: domain.Name{
-			GivenName:  name.GivenName,
-			FamilyName: name.FamilyName,
-		},
-	}
-
-	user, err := ur.CreateUser(nu)
-	return user, nil
+	return nil, errors.New("A user with this email already exists")
 }
