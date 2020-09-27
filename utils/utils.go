b092b765eee2c22a433c4beabfde2ed4ae11d2df
Neil Martin <nnmartin94@gmail.com>
2020-09-26 23:38:22 -0700

    added jwt utils funcs

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,14 @@
 package utils
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/neilnmartin/dnd5e-graphql-api/config"
+	"github.com/neilnmartin/dnd5e-graphql-api/graph/model"
+)
 
 // TODO
 
@@ -10,11 +18,38 @@ func PrettyPrint(i interface{}) string {
 	return string(s)
 }
 
-// IDTokenInput describes the user data included in the token
-type IDTokenInput struct {
+// IDToken describes the user data included in the token
+type IDToken struct {
+	ID   string
+	Name model.Name
 }
 
-// GenerateJWT takes an IDTokenInput and returns a generated json web token
-func GenerateJWT(idt IDTokenInput) {
+// GenerateJWT takes an IDToken and returns a generated json web token
+func GenerateJWT(tokenInput IDToken) (*string, error) {
+	secretKey := config.Config.IDTokenSecretKey
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["id"] = tokenInput.ID
+	claims["name"] = tokenInput.Name
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	ts, err := token.SignedString(secretKey)
+	if err != nil {
+		log.Printf("Error signing jwt: %+v", err)
+		return nil, err
+	}
+	return &ts, nil
+}
 
+//ParseJWT parses a json web token string and returns the IDToken type
+func ParseJWT(ts string) (*IDToken, error) {
+	token, err := jwt.Parse(ts, func(token *jwt.Token) (interface{}, error) {
+		return config.Config.IDTokenSecretKey, nil
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return &IDToken{
+			ID:   claims["id"].(string),
+			Name: claims["name"].(model.Name),
+		}, nil
+	}
+	return nil, err
 }
