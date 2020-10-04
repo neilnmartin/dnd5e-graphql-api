44a24c9453e4bdac2b5f43862af439e0e665ab06
Neil Martin <nnmartin94@gmail.com>
2020-10-03 23:05:47 -0700

    implemented basic create character service

--- graph/application/createCharacter.go
+++ graph/application/createCharacter.go
@@ -1,6 +1,8 @@
 package application
 
 import (
+	"errors"
+
 	"github.com/neilnmartin/dnd5e-graphql-api/graph/domain"
 )
 
@@ -8,16 +10,32 @@ import (
 type CreateCharacterInput struct {
 	race       string
 	class      string
-	subclass   string
+	subClass   string
+	subRace    string
 	background string
-	name       domain.Name
-	ur         domain.UserRepo
+	uid        string
+	name       string
 	cr         domain.CharacterRepo
 }
 
 // CreateCharacterService signs up a user by adding to the database after validating name email and password inputs
-func CreateCharacterService() (*domain.Character, error) {
-	return &domain.Character{
-		Name: "",
-	}, nil
+func CreateCharacterService(input CreateCharacterInput) (*domain.Character, error) {
+	if &input.uid != nil {
+		return nil, errors.New("Invalid user id")
+	}
+	u := domain.User{ID: input.uid}
+	c := domain.Character{
+		Name:     input.name,
+		Race:     domain.Race{Name: input.race},
+		SubRace:  domain.SubRace{Name: input.subRace},
+		Class:    domain.Class{Name: input.class},
+		SubClass: domain.SubClass{Name: input.subClass},
+	}
+	created, err := input.cr.InsertCharacter(c, u)
+	if err != nil {
+		if err == domain.ErrCharacterNotFound {
+			return nil, err
+		}
+	}
+	return created, nil
 }
